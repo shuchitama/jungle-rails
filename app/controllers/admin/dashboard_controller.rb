b4c411ee62f00b72d718b0fe3f02dae1e37aa33e
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-04-01 21:08:54 -0700

    Display stats about store

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,8 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['BASIC_AUTH_USERNAME'], password: ENV['BASIC_AUTH_PASSWORD']
 
-  def show; end
+  def show
+    @count_products = Product.count
+    @count_categories = Category.count
+  end
 end
