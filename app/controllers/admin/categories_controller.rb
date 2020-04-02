d8fe2f21734e89d4355bfc6f550faeb7b104c527
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-04-02 13:00:09 -0700

    define create action for admin/categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -10,6 +10,21 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category Created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
   end
 
 end
