1187699debc2159680bb80bb1d75176cc34da9c0
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-04-06 17:37:23 -0700

    add specs for products

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    it "saves successfully with all fields set" do
+      @category = Category.new
+      @product = Product.new(name: "Shoes", price: 2345, quantity: 19, category: @category.id)
+      @product.save
+    end
+    it "is not valid without a name" do
+      @category = Category.new
+      @product = Product.new(price: 2345, quantity: 19, category: @category.id)
+      @product.save
+      expect(@product.errors.full_messages).not_to be_empty
+    end
+    it "is not valid without a price" do
+      @category = Category.new
+      @product = Product.new(name: "Shoes", quantity: 19, category: @category.id)
+      @product.save
+      expect(@product.errors.full_messages).not_to be_empty
+    end
+    it "is not valid without a quantity" do
+      @category = Category.new
+      @product = Product.new(name: "Shoes", price: 2345, category: @category.id)
+      @product.save
+      expect(@product.errors.full_messages).not_to be_empty
+    end
+    it "is not valid without a category" do
+      @product = Product.new(name: "Shoes", price: 2345, quantity: 19)
+      @product.save
+      expect(@product.errors.full_messages).not_to be_empty
+    end
+  end
+end
