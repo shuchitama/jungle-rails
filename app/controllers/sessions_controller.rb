f6cd7ee6a56eb0bb5046b0bf5b10434832c9cb5f
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-04-06 19:03:11 -0700

    fixed bug where login wasn't working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
-    if user && user.authenticate(params[:password])
+    puts params
+    puts user_params
+    user = User.find_by_email(user_params[:email])
+    if user && user.authenticate(user_params[:password])
       session[:user_id] = user.id
       redirect_to root_path
     else
@@ -16,6 +18,11 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     redirect_to login_path
+  end
+
+  private 
+  def user_params
+    params.require(:login).permit(:email, :password)
   end
 
 end
