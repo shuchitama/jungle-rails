6a096a185cd12f64441aab62b3fef9be31806541
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-04-06 21:14:02 -0700

    fix one of the tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   describe 'Validations' do
     it "saves successfully with all fields set" do
-      @category = Category.new
-      @product = Product.new(name: "Shoes", price: 2345, quantity: 19, category: @category.id)
-      @product.save
+      @category = Category.create(name: "Category name")
+      @product = Product.create(name: "Shoes", price: 2345, quantity: 19, category: @category)
+      expect(@product.errors.full_messages).to be_empty
     end
     it "is not valid without a name" do
       @category = Category.new
