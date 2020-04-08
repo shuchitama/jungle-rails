79a60234736677451b2d52df95df0fc45d688413
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-04-08 15:01:07 -0700

    fix duplicated code

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -4,18 +4,13 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
 
   # SETUP
   before :each do
-    @category.products.create!(
-      name:  Faker::Hipster.sentence(3),
-      description: Faker::Hipster.paragraph(4),
-      image: open_asset('apparel1.jpg'),
-      quantity: 10,
-      price: 64.99
-    )
+    @category = Category.create! name: 'Apparel'
 
     10.times do |n|
       @category.products.create!(
         name:  Faker::Hipster.sentence(3),
         description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
         quantity: 10,
         price: 64.99
       )
